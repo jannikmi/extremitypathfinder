779bab33c8c49b49d9e87470624bf5996c485261
MrMinimal64 <python@michelfe.it>
2020-04-27 12:35:29 +0200

    add version file
    load version from file
    updated publishing routine

--- setup.py
+++ setup.py
@@ -1,30 +1,24 @@
 # -*- coding:utf-8 -*-
 
-import os
-import re
-
 from setuptools import setup
-
-
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `__init__.py`.
-    """
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
-
-
-version = get_version('extremitypathfinder')
 
 setup(
     name='extremitypathfinder',
-    version=version,
     packages=['extremitypathfinder'],
     description='python package for geometric shortest path computation for given 2D multi-polygon maps',
+    # version: in VERSION file https://packaging.python.org/guides/single-sourcing-package-version/
+    # With this approach you must make sure that the VERSION file is included in all your source
+    # and binary distributions (e.g. add include VERSION to your MANIFEST.in).
     author='J. Michelfeit',
     author_email='python@michelfe.it',
     license='MIT licence',
+    license_file='LICENSE',
     url='https://github.com/MrMinimal64/extremitypathfinder',  # use the URL to the github repo
+    project_urls={
+        "Source Code": "https://github.com/MrMinimal64/extremitypathfinder",
+        "Documentation": "https://github.com/MrMinimal64/extremitypathfinder/blob/master/README.rst",
+        "Changelog": "https://github.com/MrMinimal64/extremitypathfinder/blob/master/CHANGELOG.rst",
+    },
     keywords='path-planning path-finding shortest-path visibility graph visibility-graph polygon'
              'robotics navigation offline ',
     classifiers=[
