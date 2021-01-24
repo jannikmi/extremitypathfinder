bf855e337ea8634c1b94233e03be950260055388
ivan <ivanpdoria@gmail.com>
2021-01-24 18:32:49 +0000

    Improve argv parser

--- extremitypathfinder/command_line.py
+++ extremitypathfinder/command_line.py
@@ -7,13 +7,17 @@ from extremitypathfinder.helper_fcts import read_json
 environment = PlottingEnvironment()
 
 def main():
-    parser = argparse.ArgumentParser(description="description of parser")
-    parser.add_argument("json_file", type=str, help="file to be read")
-    parser.add_argument("start", type=str, help="start coordinates for search")
-    parser.add_argument("goal", type=str, help="goal coordinates for search")
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("json_file", type=str, help="JSON file to be read")
+    parser.add_argument("start", type=str, help="string containinng start coordinates as a tuple: '(2.5, 3.2)'")
+    parser.add_argument("goal", type=str, help="string containinng start coordinates as a tuple: '(7.9, 6.8)'")
+    # parser.add_argument("-h", action='store_true', help="help flag")
 
     # Takes input from sys.argv
     parsed_args = parser.parse_args()
+
+    # if(parsed_args.h):
+    #     print("blabla")
 
     # Parse JSON file
     list_of_boundaries, list_of_holes = read_json(parsed_args.json_file)
