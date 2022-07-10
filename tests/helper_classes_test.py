a949a010d7096349e6ee65a5dbe1a162af73ffff
gpapadok <gpapadokostakis@protonmail.ch>
2022-07-10 12:59:44 +0300

    Add Vertex tests

--- tests/helper_classes_test.py
+++ tests/helper_classes_test.py
@@ -4,7 +4,11 @@ import numpy as np
 import pytest
 from helpers import proto_test_case
 
-from extremitypathfinder.helper_classes import AngleRepresentation
+from extremitypathfinder.helper_classes import AngleRepresentation, Vertex
+from extremitypathfinder import helper_classes
+
+
+helper_classes.origin = Vertex((-5., -5.))
 
 
 class HelperClassesTest(unittest.TestCase):
@@ -70,6 +74,47 @@ class HelperClassesTest(unittest.TestCase):
         ]
 
         proto_test_case(data, value_test_fct)
+
+    def test_vertex_translation(self):
+        data = [
+            ([1., 1.], [6., 6.]),
+            ([0., 0.], [5., 5.]),
+            ([-12., -3.], [-7., 2.]),
+            ([-4., 5.], [1., 10.]),
+            ([3., -2.], [8., 3.]),
+        ]
+
+        def translation_test_fct(input):
+            return Vertex(input).get_coordinates_translated().tolist()
+
+        proto_test_case(data, translation_test_fct)
+
+    def test_vertex_angle_repr(self):
+        data = [
+            ([0., -5.], 0.),
+            ([-5., 0.], 1.),
+            ([-6., -5.], 2.),
+            ([-5., -6.], 3.),
+        ]
+
+        def angle_repr_test_fct(input):
+            return Vertex(input).get_angle_representation()
+
+        proto_test_case(data, angle_repr_test_fct)
+
+    def test_vertex_distance_to_origin(self):
+        data = [
+            ([0., 0.], np.sqrt(50)),
+            ([-5., 0.], 5.),
+            ([0., -5], 5.),
+            ([-3., -2.], np.sqrt(13)),
+            ([2., 5.], np.sqrt(149)),
+        ]
+
+        def dist_to_origin_test_fct(input):
+            return Vertex(input).get_distance_to_origin()
+
+        proto_test_case(data, dist_to_origin_test_fct)
 
 
 if __name__ == "__main__":
